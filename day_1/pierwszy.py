05672d7b466258d82be0dd1fc0c291af58c02e03
SzlachtaP <patryk.szla@interia.pl>
2025-03-24 11:44:35 +0100

    init

---
+++ day_1/pierwszy.py
@@ -0,0 +1,20 @@
+import sys
+
+print('czesc')  # trestt
+print('dwa')
+print('dwa')
+print('dwa')
+print('dwa')
+
+print(type('TEST'))
+
+print('39' + '14')
+print(type(22))
+
+print(int('39') + 14)  # rzutowanie na typ
+
+print(5 * '4')
+print(25 * '168')
+print(25 * 167)
+print(sys.int_info)
+
