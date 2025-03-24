7e3871b34fdfc05d49ebcf14121e1858e98e3da7
SzlachtaP <patryk.szla@interia.pl>
2025-03-24 15:32:21 +0100

    data types 3

---
+++ day_1/typy_danych_3_krotka.py
@@ -0,0 +1,11 @@
+tupla_imiona = ("Radek", "Tomek", "Zenek", "Anna", "Marek")
+print(tupla_imiona)
+print(type(tupla_imiona))
+
+tupla_liczby = 43,55,22,33,34,11,100
+print(type(tupla_liczby))
+print(tupla_liczby)
+tupla = ("Radek")
+print(type(tupla))
+tupla = "Radek",
+print(type(tupla))
