6eb97291fc0a6da8b628c90386fd382aca5dcb84
SzlachtaP <patryk.szla@interia.pl>
2025-03-25 12:58:01 +0100

    conditional statements 2

--- day_02/warunki.py
+++ day_02/warunki.py
@@ -32,26 +32,82 @@ n = len(a)
 if n > 3:
     print(f"Długość wynosi: {n}, więcej niż 3")
 
-
 # Operator mors, walrus operator :=
-if(n := len(a)) > 3:
+if (n := len(a)) > 3:
     print(f"Długość wynosi: {n}, więcej niż 3")
 
-podatek = 0
-zarobki = int(input("Podaj zarobki"))
+# podatek = 0
+# zarobki = int(input("Podaj zarobki"))
+#
+# if zarobki < 10000:
+#     podatek = 0
+# elif zarobki < 40000:
+#     podatek = 0.2
+# elif zarobki < 100000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
 
-if zarobki < 10000:
-    podatek = 0
-elif zarobki < 40000:
-    podatek = 0.2
-elif zarobki < 100000:
-    podatek = 0.4
+
+# print(f"Podatek wynosi {podatek * zarobki}")
+
+
+# podatek 0.2 dla zarobkow od 10000 do 39999
+
+
+suma_zam = 150
+if suma_zam > 100:
+    rabat = 25
 else:
-    podatek = 0.9
+    rabat = 0
 
+print(f"Rabat wynosi {rabat}")
 
-print(f"Podatek wynosi {podatek * zarobki}")
+rabat = 25 if suma_zam > 100 else 0
+print(f"Rabat wynosi {rabat}")
 
+# Zasymulujemy system logów
+# Zmienna będzie pzechowywać informacje jaki system wysłał log
+# email, console, inny
+# gdy alert z systemu console -> "Stało się coś strasznego"
+# gdy alert z systemu email -> "System email"
+# gdy alert mail -> druga zmienna przechowująca error, medium inny
+# Nalezy opis bledu dodac do listy bledow
+# eror -> krytyczny
 
-#podatek 0.2 dla zarobkow od 10000 do 39999
+log = ''
+error = "error"
+errors = list()
+alert = input("jaki log")
+if alert == 'console':
+    log = "Stało się coś strasznego"
+elif alert == 'email':
+    if error == "error":
+        errors.append("Krytyczny")
+    elif error == "medium":
+        errors.append("Ostrzeżenie")
+    else:
+        errors.append("Inny")
+    log = "system email"
+else:
+    log = "inny"
 
+print(log)
+print(errors)
+
+odp = input("Podaj datę chrztu Polski (rok)")
+punkty = 0
+if odp.strip().casefold() == "966":
+    print("Dobrze")
+    punkty += 1
+else:
+    print("Musisz się dalej uczyć")
+
+odp = input("Stolica Polski")
+if odp.strip().casefold() == "Warszawa".casefold():
+    print("Dobrze")
+    punkty += 1
+else:
+    print("Musisz się dalej uczyć")
+
+print(f"Twój wynik {punkty}")
