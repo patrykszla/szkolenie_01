a98d7f0b4c80c2fae87c07eaef54a92bdc7bd6ec
SzlachtaP <patryk.szla@interia.pl>
2025-03-26 14:27:59 +0100

    math 1

---
+++ day_03/matematyka.py
@@ -0,0 +1,10 @@
+import math
+
+print(math.pi)
+print(math.sqrt(25))
+
+angle_degress =30
+angle_radians = math.radians(angle_degress)
+print(angle_radians)
+sin_val = math.sin(angle_radians)
+print(sin_val)
